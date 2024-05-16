20b5b308534c9a95fa98afeb0c9fab23d2d8d068
Martin Beacham <martinlutherbeacham@gmail.com>
2024-05-16 10:44:11 -0400

    Update brickMath.js to remove duplicate function

--- brickMath.js
+++ brickMath.js
@@ -8,15 +8,6 @@ return acc+curr;
   
 }
 
-function countAvailBricks() {
-
-return Array.from(document.querySelectorAll("[data-test=pab-quantity-in-set]")).reduce((acc, curr)=>{
-curr = curr.innerText
-curr = parseInt(curr.replace('Qty in set: ',''))
-return acc+curr;
-},0)
-}
-
 function printPriceByBlock(){
 
 	document.querySelectorAll(".ElementsList_leaf__3tVNf").forEach((item)=>{
