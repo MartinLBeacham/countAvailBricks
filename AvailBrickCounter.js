4ea0295e29ce335023181b0d362a726c527d7400
Martin Beacham <martinlutherbeacham@gmail.com>
2024-05-16 10:42:09 -0400

    Update and rename AvailBrickCounter.js to brickMath.js

--- AvailBrickCounter.js
+++
@@ -1,9 +0,0 @@
-function countAvailBricks() {
-
-return Array.from(document.querySelectorAll("[data-test=pab-quantity-in-set]")).reduce((acc, curr)=>{
-curr = curr.innerText
-curr = parseInt(curr.replace('Qty in set: ',''))
-return acc+curr;
-},0)
-  
-}
---
+++ brickMath.js
@@ -0,0 +1,44 @@
+function countAvailBricks() {
+
+return Array.from(document.querySelectorAll("[data-test=pab-quantity-in-set]")).reduce((acc, curr)=>{
+curr = curr.innerText
+curr = parseInt(curr.replace('Qty in set: ',''))
+return acc+curr;
+},0)
+  
+}
+
+function countAvailBricks() {
+
+return Array.from(document.querySelectorAll("[data-test=pab-quantity-in-set]")).reduce((acc, curr)=>{
+curr = curr.innerText
+curr = parseInt(curr.replace('Qty in set: ',''))
+return acc+curr;
+},0)
+}
+
+function printPriceByBlock(){
+
+	document.querySelectorAll(".ElementsList_leaf__3tVNf").forEach((item)=>{
+	
+	const count = parseInt(item.querySelector("[data-test=pab-quantity-in-set]").innerText.replace('Qty in set: ',''));
+	
+	const price = item.querySelector("[data-test=pab-item-price]").innerText.replace('$','');
+	
+	console.log(price*count);
+})
+
+} 
+
+function totalPrice(){
+
+	return Array.from(document.querySelectorAll(".ElementsList_leaf__3tVNf")).reduce((acc,curr)=>{
+	
+	const count = parseInt(curr.querySelector("[data-test=pab-quantity-in-set]").innerText.replace('Qty in set: ',''));
+	
+	const price = curr.querySelector("[data-test=pab-item-price]").innerText.replace('$','');
+	
+	return price*count+acc
+},0)
+
+} 
